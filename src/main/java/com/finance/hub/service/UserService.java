f89b67fb6280b0284442e77298e7e2ba24d8ce11
Youri Dos Santos <yourimoutinho@gmail.com>
2025-10-15 17:20:22 -0700

    implemented the checklist but have not tested it yet

---
+++ src/main/java/com/finance/hub/service/UserService.java
@@ -0,0 +1,38 @@
+package com.finance.hub.service;
+
+import com.finance.hub.model.Role;
+import com.finance.hub.model.User;
+import com.finance.hub.projection.UserDetailsProjection;
+import com.finance.hub.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if(result.size() == 0) {
+            throw new UsernameNotFoundException("User not Found");
+        }
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+
+        for(UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
